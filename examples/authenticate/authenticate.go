77edac1de1da0f3e98a27a624cc72d0600ce6bba
Zac Bergquist <zbergquist99@gmail.com>
2016-03-19 15:20:27 -0400

    Add authentication example
    (See #8)

---
+++ examples/authenticate/authenticate.go
@@ -0,0 +1,65 @@
+// This example demonstrates how to authenticate with Spotify.
+// In order to run this example yourself, you'll need to:
+//
+//  1. Register an application at: https://developer.spotify.com/my-applications/
+//       - Use "http://localhost:8080/callback" as the redirect URI
+//  2. Set the SPOTIFY_ID environment variable to the client ID you got in step 1.
+//  3. Set the SPOTIFY_SECRET environment variable to the client secret from step 1.
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/zmb3/spotify"
+)
+
+// redirectURI is the OAuth redirect URI for the application.
+// You must register an application at Spotify's developer portal
+// and enter this value.
+const redirectURI = "http://localhost:8080/callback"
+
+var (
+	auth  = spotify.NewAuthenticator(redirectURI, spotify.ScopeUserReadPrivate)
+	ch    = make(chan *spotify.Client)
+	state = "abc123"
+)
+
+func main() {
+	// first start an HTTP server
+	http.HandleFunc("/callback", completeAuth)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Got request for:", r.URL.String())
+	})
+	go http.ListenAndServe(":8080", nil)
+
+	url := auth.AuthURL(state)
+	fmt.Println("Please log in to Spotify by visiting the following page in your browser:", url)
+
+	// wait for auth to complete
+	client := <-ch
+
+	// use the client to make calls that require authorization
+	user, err := client.CurrentUser()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("You are logged in as:", user.ID)
+}
+
+func completeAuth(w http.ResponseWriter, r *http.Request) {
+	tok, err := auth.Token(state, r)
+	if err != nil {
+		http.Error(w, "Couldn't get token", http.StatusForbidden)
+		log.Fatal(err)
+	}
+	if st := r.FormValue("state"); st != state {
+		http.NotFound(w, r)
+		log.Fatalf("State mismatch: %s != %s\n", st, state)
+	}
+	// use the token to get an authenticated client
+	client := auth.NewClient(tok)
+	fmt.Fprintf(w, "Login Completed!")
+	ch <- &client
+}
--- examples/search/search.go
+++ examples/search/search.go
@@ -2,28 +2,30 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/zmb3/spotify"
 )
 
 func main() {
-	// search for "holiday"
+	// search for playlists and albums containing "holiday"
 	results, err := spotify.Search("holiday", spotify.SearchTypePlaylist|spotify.SearchTypeAlbum)
 	if err != nil {
-		fmt.Printf("Error occurred: %v\n", err)
-		return
+		log.Fatal(err)
 	}
 
 	// handle album results
 	if results.Albums != nil {
+		fmt.Println("Albums:")
 		for _, item := range results.Albums.Albums {
-			fmt.Println("Album: ", item.Name)
+			fmt.Println("   ", item.Name)
 		}
 	}
 	// handle playlist results
 	if results.Playlists != nil {
+		fmt.Println("Playlists:")
 		for _, item := range results.Playlists.Playlists {
-			fmt.Println("Playlist: ", item.Name)
+			fmt.Println("   ", item.Name)
 		}
 	}
 }
