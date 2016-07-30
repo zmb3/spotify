7c329e568850060851710e4cfecc3aa70c56ddf7
Zac Bergquist <zbergquist99@gmail.com>
2016-07-29 21:16:20 -0600

    Remove license note from each file

--- playlist.go
+++ playlist.go
@@ -1,17 +1,3 @@
-// Copyright 2014, 2015 Zac Bergquist
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package spotify
 
 import (
@@ -37,7 +23,7 @@ type PlaylistTracks struct {
 type SimplePlaylist struct {
 	// Indicates whether the playlist owner allows others to modify the playlist.
 	// Note: only non-collaborative playlists are currently returned by Spotify's Web API.
-	Collaborative bool        `json:"collaborative"`
+	Collaborative bool              `json:"collaborative"`
 	ExternalURLs  map[string]string `json:"external_urls"`
 	// A link to the Web API endpoint providing full details of the playlist.
 	Endpoint string `json:"href"`
