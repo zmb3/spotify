2fef3f1b126ff3efcc36e3c3d521f1fc54c5a91c
Dustin Oprea <myselfasunder@gmail.com>
2017-05-29 13:58:00 -0400

    Adjusted test to encapsulate played-at time.

--- player_test.go
+++ player_test.go
@@ -97,6 +97,13 @@ func TestPlayerRecentlyPlayed(t *testing.T) {
 	if len(items) != 20 {
 		t.Error("Too few or too many items were returned")
 	}
+
+	actualTimePhrase := items[0].PlayedAt.Format("2006-01-02T15:04:05.999Z")
+	expectedTimePhrase := "2017-05-27T20:07:54.721Z"
+
+	if actualTimePhrase != expectedTimePhrase {
+		t.Errorf("Time of first track was not parsed correctly: [%s] != [%s]", actualTimePhrase, expectedTimePhrase)
+	}
 }
 
 func TestPlayArgsError(t *testing.T) {
