a89a48f82448aec19fa2188ff4bd39a1ef93b2da
Petter Karlsrud <petterkarlsrud@me.com>
2017-11-13 18:48:23 +0100

    Added a slice of SimpleArtists to SimpleAlbum
    - Added a slice of SimpleArtists to the SimpleAlbum struct

--- album.go
+++ album.go
@@ -13,6 +13,8 @@ import (
 type SimpleAlbum struct {
 	// The name of the album.
 	Name string `json:"name"`
+	// An array of SimplifiedArtists
+	Artists []SimpleArtist `json:"artists"`
 	// The type of the album: one of "album",
 	// "single", or "compilation".
 	AlbumType string `json:"album_type"`
