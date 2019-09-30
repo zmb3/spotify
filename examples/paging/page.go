c23dd5d1a0c93cd720de456b50054498b87bad4d
Zac Bergquist <zbergquist99@gmail.com>
2019-09-30 11:14:22 -0600

    Add convenience functions to move to prev/next page
    This closes #75 and closes #79.

---
+++ examples/paging/page.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/zmb3/spotify"
+	"golang.org/x/oauth2/clientcredentials"
+)
+
+func main() {
+	config := &clientcredentials.Config{
+		ClientID:     os.Getenv("SPOTIFY_ID"),
+		ClientSecret: os.Getenv("SPOTIFY_SECRET"),
+		TokenURL:     spotify.TokenURL,
+	}
+	token, err := config.Token(context.Background())
+	if err != nil {
+		log.Fatalf("couldn't get token: %v", err)
+	}
+
+	client := spotify.Authenticator{}.NewClient(token)
+
+	tracks, err := client.GetPlaylistTracks("37i9dQZF1DWWzVPEmatsUB")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Playlist has %d total tracks", tracks.Total)
+	for page := 1; ; page++ {
+		log.Printf("  Page %d has %d tracks", page, len(tracks.Tracks))
+		err = client.NextPage(tracks)
+		if err == spotify.ErrNoMorePages {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
