df2d9db1a5381ef233fce033d42e7827b38d968d
ahmed-deftoner <ahmedghtwhts786@gmail.com>
2023-04-24 13:30:18 +0500

    add track attributes and multiple artist ID's

--- examples/recommendation/recommendation.go
+++ examples/recommendation/recommendation.go
@@ -28,28 +28,41 @@ func main() {
 	httpClient := spotifyauth.New().Client(ctx, token)
 	client := spotify.New(httpClient)
 
-	// search for albums with the name Sempiternal
-	artists, err := client.Search(ctx, "Invent Animate", spotify.SearchTypeArtist)
-	if err != nil {
-		log.Fatal(err)
+	// store artist and genre info (upto 5 seed values allowed)
+	favorite_artists := []string{"Invent Animate", "Spiritbox", "Sleep Token"}
+	genres := []string{"metalcore"}
+	var artist_ids []spotify.ID
+
+	// find the ID for each artist and append it to list of artist ID's
+	for _, v := range favorite_artists {
+		artists, err := client.Search(ctx, v, spotify.SearchTypeArtist)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		artist_ids = append(artist_ids, artists.Artists.Artists[0].ID)
 	}
 
-	// select the top album
-	artist := artists.Artists.Artists[0]
-
+	// store the values in seed
 	seeds := spotify.Seeds{
-		Artists: []spotify.ID{artist.ID},
-		Genres:  []string{"metalcore", "hardcore"},
+		Artists: artist_ids,
+		Genres:  genres,
 	}
 
-	res, err := client.GetRecommendations(ctx, seeds, nil)
+	// declare track attributes for a more refined search
+	track_attributes := spotify.NewTrackAttributes().
+		MaxValence(0.4).
+		TargetEnergy(0.6).
+		TargetDanceability(0.6)
+
+	// get recommendations based on seed values
+	res, err := client.GetRecommendations(ctx, seeds, track_attributes, spotify.Country("US"), spotify.Limit(10))
 	if err != nil {
 		log.Fatal(err)
 	}
-	for _, rs := range res.Seeds {
-		fmt.Println(rs)
-	}
+
+	// display the recommended tracks along with artists
 	for _, st := range res.Tracks {
-		fmt.Println(st.Name + " - " + st.Artists[0].Name)
+		fmt.Println(st.Name + " by " + st.Artists[0].Name)
 	}
 }
