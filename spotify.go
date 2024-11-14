ccb251b1fd8d56d383fa801a30c8024aa52dd281
Jules <55190371+Pineapple217@users.noreply.github.com>
2024-11-14 21:01:56 +0100

    fix spelling
    Co-authored-by: Jonathan Hall <jonathan@jhall.io>

--- spotify.go
+++ spotify.go
@@ -79,7 +79,7 @@ func WithAcceptLanguage(lang string) ClientOption {
 	}
 }
 
-// WithMaxRetryDuration limits the amount of time that the client wil wait to retry afther being rate limited.
+// WithMaxRetryDuration limits the amount of time that the client will wait to retry after being rate limited.
 // If the retry time is longer than the max, then the client will return an error.
 // This option only works when auto retry is enabled
 func WithMaxRetryDuration(duration time.Duration) ClientOption {
